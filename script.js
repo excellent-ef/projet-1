d56608c02898c3287c4ffdf96523176bbe69e8ea
excellent_ef <heliac.ef@gmail.com>
2021-02-18 18:07:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// let ageOld = 1;
+// const Name = "Wella";
+
+// ageOld = 2;
+
+// console.log(Name);
+
+// const hair = {
+//     a: "black"
+// };
+
+// hair.a = "silver";
+
+// console.log(hair);
+// alert('Грусти, понел');
+
+// const result = confirm("You are here?")
+
+// console.log(result);
+
+// const answer = +prompt("Опять бездельнечаешь?", "Ага");
+// console.log(5 + answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Какая ваша фамилия?');
+// answers[2] = prompt('Сколько вам лет?');
+
+// console.log(answers);
+
+
+console.log(+"8" + 5)
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(10%2);
+
+console.log(5*4 === 20);
